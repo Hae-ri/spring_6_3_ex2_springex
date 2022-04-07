2aa07d0ba59787b7ccd78c118785243ef852b8f9
ici806 <ici806@DESKTOP-DF4NS8A>
2022-04-07 16:39:30 +0900

    자바 컨테이너 설정파일 안에 xml 컨테이너 설정파일 넣기

---
+++ src/main/java/com/javatest/ex/ApplicationConfig.java
@@ -0,0 +1,44 @@
+package com.javatest.ex;
+
+import java.util.ArrayList;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.ImportResource;
+
+@Configuration
+@ImportResource("classpath:applicationCTX.xml")
+public class ApplicationConfig {
+
+	@Bean
+	public Student student1() {
+		ArrayList<String> hobbys = new ArrayList<String>();
+		hobbys.add("수영");
+		hobbys.add("달리기");
+		hobbys.add("노래");
+		
+		Student student = new Student("홍길동","30세",hobbys); // 생성자
+		
+		student.setHeight(180); // 프로퍼티
+		student.setWeight(90);
+		
+		return student;
+	}
+	
+	@Bean
+	public Student student2() {
+		ArrayList<String> hobbys = new ArrayList<String>();
+		hobbys.add("활쏘기");
+		hobbys.add("말타기");
+		hobbys.add("일기쓰기");
+		
+		Student student = new Student("이순신","50세",hobbys); // 생성자
+		
+		student.setHeight(190); // 프로퍼티
+		student.setWeight(90);
+		
+		return student;
+	} 
+	
+	
+}
---
+++ src/main/java/com/javatest/ex/MainClass.java
@@ -0,0 +1,26 @@
+package com.javatest.ex;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.AbstractApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		
+		// 컨테이너 객체를 ApplicationConfig 클래스로 설정하여 생성
+		AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext(ApplicationConfig.class);
+
+		
+		Student student1 = ctx.getBean("student1",Student.class);
+		Student student2 = ctx.getBean("student2",Student.class);
+		Student student3 = ctx.getBean("student3",Student.class);
+		
+		System.out.println(student1.getName()); // 홍길동
+		System.out.println(student2.getName()); // 이순신
+		System.out.println(student3.getName()); // 김유신 
+
+		ctx.close();
+	}
+
+}
